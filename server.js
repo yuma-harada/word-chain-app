e28c396e794d486eab1a3e5d19efea4564583fda
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2025-06-06 15:07:58 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+// server.js
+import { serveDir } from "jsr:@std/http/file-server";
+
+let previousWord = "しりとり";
+// localhostにDenoのHTTPサーバーを展開
+Deno.serve(async (_req) => {
+    // パス名を取得する
+    // http://localhost:8000/hoge に接続した場合"/hoge"が取得できる
+    const pathname = new URL(_req.url).pathname;
+    console.log(`pathname: ${pathname}`);
+
+    // GET /shiritori: 直前の単語を返す
+    if (_req.method === "GET" && pathname === "/shiritori") {
+        return new Response(previousWord);
+    }
+
+    // POST /shiritori: 次の単語を受け取って保存する
+    if (_req.method === "POST" && pathname === "/shiritori") {
+        // リクエストのペイロードを取得
+        const requestJson = await _req.json();
+        // JSONの中からnextWordを取得
+        const nextWord = requestJson["nextWord"];
+
+        // previousWordの末尾とnextWordの先頭が同一か確認
+        if (previousWord.slice(-1) === nextWord.slice(0, 1)) {
+            // 同一であれば、previousWordを更新
+            previousWord = nextWord;
+        } // 同一でない単語の入力時に、エラーを返す
+        else {
+            return new Response(
+                JSON.stringify({
+                    "errorMessage": "前の単語に続いていません",
+                    "errorCode": "10001",
+                }),
+                {
+                    status: 400,
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                    },
+                },
+            );
+        }
+
+        // 現在の単語を返す
+        return new Response(previousWord);
+    }
+    // ./public以下のファイルを公開
+    return serveDir(
+        _req,
+        {
+            /*
+            - fsRoot: 公開するフォルダを指定
+            - urlRoot: フォルダを展開するURLを指定。今回はlocalhost:8000/に直に展開する
+            - enableCors: CORSの設定を付加するか
+            */
+            fsRoot: "./public/",
+            urlRoot: "",
+            enableCors: true,
+        },
+    );
+});
