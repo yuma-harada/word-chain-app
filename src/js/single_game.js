bace048d45a4b4244c0688cfa6ef44f5a9f1f137
yuma-harada <yu2ma1da7ha0ra@gmail.com>
2025-06-08 19:54:30 +0900

    refac: 配列を受け渡す形式に変更

--- src/js/single_game.js
+++ src/js/single_game.js
@@ -1,12 +1,9 @@
 globalThis.onload = async () => {
   // GET /shiritoriを実行
   const response = await fetch("/shiritori", { method: "GET" });
-  // responseの中からレスポンスのテキストデータを取得
-  const previousWord = await response.text();
-  // id: previousWordのタグを取得
+  const words = await response.json();
   const paragraph = document.querySelector("#previousWord");
-  // 取得したタグの中身を書き換える
-  paragraph.innerHTML = `前の単語: ${previousWord}`;
+  paragraph.innerHTML = `前の単語: ${words.slice(-1)[0]}`;
 };
 // 送信ボタンの押下時に実行
 document.querySelector("#nextWordSendButton").onclick = async () => {
@@ -31,12 +28,12 @@ document.querySelector("#nextWordSendButton").onclick = async () => {
     alert(errorObj["errorMessage"]);
   }
 
-  const previousWord = await response.text();
+  const words = await response.json();
 
   // id: previousWordのタグを取得
   const paragraph = document.querySelector("#previousWord");
   // 取得したタグの中身を書き換える
-  paragraph.textContent = `前の単語: ${previousWord}`;
+  paragraph.textContent = `前の単語: ${words.slice(-1)[0]}`;
   // inputタグの中身を消去する
   nextWordInput.value = "";
 };
@@ -49,9 +46,9 @@ document.querySelector("#resetButton").onclick = async () => {
       headers: { "Content-Type": "application/json" },
     },
   );
-  const previousWord = await response.text();
+  const words = await response.json();
 
   const paragraph = document.querySelector("#previousWord");
-  paragraph.textContent = `前の単語: ${previousWord}`;
+  paragraph.textContent = `前の単語: ${words.slice(-1)[0]}`;
   nextWordInput.value = "";
 };
